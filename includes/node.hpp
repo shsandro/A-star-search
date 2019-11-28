5323d13855d9a26d4c81965a7273777784605bca
shsandro <shsandro@github.com>
2019-11-28 00:17:19 -0300

    update node.hpp

--- includes/node.hpp
+++ includes/node.hpp
@@ -10,13 +10,14 @@ public:
     int g;
     float h;
     float f;
-    Board board;
+    Board *board;
     Node *father;
 
 public:
+    float make_heuristic(Node &);
     bool check_success();
-    Node();
-    Node(Node &);
+    Node(Board *);
+    Node(Node &, Board *);
 };
 
 void print_board(Board &);

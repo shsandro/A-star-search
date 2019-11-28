9fb803645dee5a3f84bfb6f5778ddb12e5503cf5
shsandro <shsandro@github.com>
2019-11-28 00:16:42 -0300

    update game.hpp

--- includes/game.hpp
+++ includes/game.hpp
@@ -1,20 +1,32 @@
 #pragma once
 
 #include <iostream>
-#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <algorithm>
 #include "board.hpp"
 #include "node.hpp"
+
+struct CompareSecond
+{
+    bool operator()(const std::pair<std::string, Node *> &left, const std::pair<std::string, Node *> &right) const
+    {
+        return left.second->f > right.second->f;
+    }
+};
 
 class Game
 {
 public:
-    std::vector<Node *> open_list;
-    std::vector<Node *> closed_list;
+    std::priority_queue<std::pair<std::string, Node *>, std::deque<std::pair<std::string, Node *>>, CompareSecond> heap;
+    std::unordered_map<std::string, Node *> open_list;
+    std::unordered_map<std::string, Node *> closed_list;
 
 public:
     void make_nexts(Node *);
-    bool check_open_list(Node &);
-    bool check_closed_list(Node &);
-    bool check_closed_open(Node &);
-    Game();
+    void check_open_list(Node &, std::string);
+    bool check_closed_list(Node &, std::string);
+    bool check_a_f(Node &, std::string);
+    Game(Board *);
 };
